4d452786caa723afa1e64afa49e52bef99ab53a4
Tohasim <toreh.simonsen@live.dk>
2023-09-20 06:43:02 +0200

    Cars'r'us ready for frontend

--- src/main/java/dat3/cars/api/ReservationController.java
+++ src/main/java/dat3/cars/api/ReservationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/reservations")
 public class ReservationController {
     ReservationService service;
