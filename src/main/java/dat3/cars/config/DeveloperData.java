26ff68beca380b0d997452f98c88961dd4ac992d
Tohasim <toreh.simonsen@live.dk>
2023-09-28 19:17:11 +0200

    Added some dummy members with different roles

--- src/main/java/dat3/cars/config/DeveloperData.java
+++ src/main/java/dat3/cars/config/DeveloperData.java
@@ -6,6 +6,7 @@ import dat3.cars.entity.Reservation;
 import dat3.cars.repositories.CarRepository;
 import dat3.cars.repositories.MemberRepository;
 import dat3.cars.repositories.ReservationRepository;
+import dat3.security.entity.Role;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Controller;
@@ -140,5 +141,59 @@ public class DeveloperData implements ApplicationRunner {
         // Now, the 'reservations' list contains 10 reservations with hardcoded values for cars and members.
 
         reservationRepository.saveAll(reservations);
+
+        createDummyUsers();
+    }
+
+    private void createDummyUsers() {
+        String passwordUsedByAll = "1234";
+        Member member1 = new Member(
+                "user1",
+                passwordUsedByAll,
+                "email1",
+                "firstname1",
+                "lastName1",
+                "street1",
+                "city1",
+                "zip1"
+        );
+        Member member2 = new Member(
+                "user2",
+                passwordUsedByAll,
+                "email2",
+                "firstname2",
+                "lastName2",
+                "street2",
+                "city2",
+                "zip2"
+        );
+        Member member3 = new Member(
+                "user3",
+                passwordUsedByAll,
+                "email3",
+                "firstname3",
+                "lastName3",
+                "street3",
+                "city3",
+                "zip3"
+        );
+        Member member4 = new Member(
+                "user4",
+                passwordUsedByAll,
+                "email4",
+                "firstname4",
+                "lastName4",
+                "street4",
+                "city4",
+                "zip4"
+        );
+
+        member1.addRole(Role.ADMIN);
+        member2.addRole(Role.USER);
+        member3.addRole(Role.ADMIN);
+        member3.addRole(Role.USER);
+        ArrayList<Member> members = new ArrayList<>( List.of(member1, member2, member3, member4));
+        memberRepository.saveAll(members);
+
     }
 }
